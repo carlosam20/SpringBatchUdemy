9c54a7a0313e1193b45f9231f6bbe0d93caf773d
Carlos Abuin <carlos.abuin@nter.es>
2026-02-06 12:11:24 +0100

    fix: Exception handling && Spring IoC DI tags for Batch Config

--- src/main/java/com/example/SpringBatchUdemy/config/SensorDataProcessor.java
+++ src/main/java/com/example/SpringBatchUdemy/config/SensorDataProcessor.java
@@ -3,15 +3,28 @@ package com.example.SpringBatchUdemy.config;
 import com.example.SpringBatchUdemy.MathTempServiceImpl;
 import com.example.SpringBatchUdemy.dto.InputSensorDataDTO;
 import com.example.SpringBatchUdemy.dto.OutputXMLSensorDataDTO;
+import com.example.SpringBatchUdemy.mapper.TemperatureMappingConverter;
+import lombok.NonNull;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
 
 
+@Component
     public class SensorDataProcessor implements ItemProcessor<InputSensorDataDTO, OutputXMLSensorDataDTO> {
-        public OutputXMLSensorDataDTO process(InputSensorDataDTO isd) throws Exception {
-            MathTempServiceImpl mathTempService = new MathTempServiceImpl();
-            return mathTempService.tempOperation(isd);
+    Log log = LogFactory.getLog(TemperatureMappingConverter.class);
+    @Override
+        public @NonNull OutputXMLSensorDataDTO process(InputSensorDataDTO isd){
+        if(isd.temps() == null|| isd.temps().isEmpty() ||  isd.localDate() == null){
+            throw new RuntimeException("InputDTO has empty values");
         }
+        MathTempServiceImpl mathTempService = new MathTempServiceImpl();
+        return mathTempService.tempOperation(isd);
+
+
+    }
     }
 
 
